14cad749123613929a86fa02264638adaf1eef31
Denis Georgiev <d.georgiev.91@gmail.com>
2013-10-13 23:31:52 -0500

    Changes made by Denis Georgiev (d.georgiev.91@gmail.com).

---
+++ Viner/scripts/app/view-models/home-view-model.js
@@ -0,0 +1,19 @@
+var app = app || {};
+
+(function(a) {
+    var viewModel = kendo.observable({
+        vines:[],
+    });
+    
+    function init(e) {
+        kendo.bind(e.view.element, viewModel);
+        httpRequester.getJSON( app.servicesBaseUrl + "vines/all")
+        .then(function (vines) {
+            viewModel.set("vines", vines);            
+        });        
+    }
+    
+    a.homeViewModel = {
+        init:init          
+    };
+}(app));
